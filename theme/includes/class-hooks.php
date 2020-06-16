4332dc7b4fde38c642e067585bf4877d62853341
Paul Schreiber <paulschreiber@gmail.com>
2020-06-16 17:40:56 -0400

    remove extra parameter from wp_enqueue_style() call

--- theme/includes/class-hooks.php
+++ theme/includes/class-hooks.php
@@ -22,7 +22,7 @@ class Hooks {
 		$type = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? 'src' : 'min';
 
 		wp_enqueue_script( 'main', get_template_directory_uri() . "/assets/js/main.{$type}.js", [ 'jquery', 'underscore', 'wp-util' ], THEME_VERSION, true );
-		wp_enqueue_style( 'main', get_template_directory_uri() . "/assets/css/main.{$type}.css", [], THEME_VERSION, false );
+		wp_enqueue_style( 'main', get_template_directory_uri() . "/assets/css/main.{$type}.css", [], THEME_VERSION );
 		wp_enqueue_style( 'source-sans', 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&display=swap', [], THEME_VERSION );
 	}
 }
