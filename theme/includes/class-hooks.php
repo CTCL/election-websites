10bdeef8fcf55399c8dd307e13c6070a97528e6d
Paul Schreiber <paulschreiber@gmail.com>
2020-06-16 17:41:22 -0400

    add thumbnail and menu support to theme

--- theme/includes/class-hooks.php
+++ theme/includes/class-hooks.php
@@ -25,6 +25,22 @@ class Hooks {
 		wp_enqueue_style( 'main', get_template_directory_uri() . "/assets/css/main.{$type}.css", [], THEME_VERSION );
 		wp_enqueue_style( 'source-sans', 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&display=swap', [], THEME_VERSION );
 	}
+
+	/**
+	 * Set up theme defaults and register supported WordPress features.
+	 */
+	public static function configure_theme() {
+
+		$menus = [
+			'header-menu' => 'Top Nav Menu',
+		];
+
+		register_nav_menus( $menus );
+
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'title-tag' );
+	}
 }
 
 add_action( 'after_setup_theme', [ '\CTCL\ElectionWebsite\Hooks', 'setup_hooks' ] );
+add_action( 'after_setup_theme', [ '\CTCL\ElectionWebsite\Hooks', 'configure_theme' ] );
