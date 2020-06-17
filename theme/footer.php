23f436eede74d8f5de7dd95308ec52679d910b42
legarlin <legarlin@gmail.com>
2020-06-17 16:48:10 -0700

    style abbreviated footer

--- theme/footer.php
+++ theme/footer.php
@@ -7,30 +7,37 @@
  */
 ?>
 <footer>
-	<div class="footer-content-wrapper">
-		<div>
-			<h3 class="section-title"><?php bloginfo( 'title' ); ?></h3>
-			<p><b>Gregory J. Diaz,</b> Registrar of Voters</p>
-			<p><b>Email:</b> office@nevadacountyvotes.gov</p>
-			<p><b>Phone:</b> 415.555.5555</p>	
+	<?php if (is_front_page()) { ?>
+		<div class="footer-content-wrapper">
+			<div>
+				<h3 class="section-title"><?php bloginfo( 'title' ); ?></h3>
+				<p><b>Gregory J. Diaz,</b> Registrar of Voters</p>
+				<p><b>Email:</b> office@nevadacountyvotes.gov</p>
+				<p><b>Phone:</b> 415.555.5555</p>	
+			</div>
+			<div>
+				<p class="section-title"><b>Support</b></p>
+				<?php
+					wp_nav_menu([
+						'theme_location' => 'footer-section-1-menu'
+					]);
+				?>
+			</div>
+			<div>
+				<p class="section-title"><b>Campaign Resources</b></p>
+				<?php
+					wp_nav_menu([
+						'theme_location' => 'footer-section-2-menu'
+					]);
+				?>
+			</div>
 		</div>
-		<div>
-			<p class="section-title"><b>Support</b></p>
-			<?php
-				wp_nav_menu([
-					'theme_location' => 'footer-section-1-menu'
-				]);
-			?>
-		</div>
-		<div>
-			<p class="section-title"><b>Campaign Resources</b></p>
-			<?php
-				wp_nav_menu([
-					'theme_location' => 'footer-section-2-menu'
-				]);
-			?>
-		</div>
-	</div>
+	<?php } else { ?>
+		<div class="abbreviated-footer">
+			<h3><?php bloginfo( 'title' ); ?></h3>
+			<p><b>Email:</b> office@nevadacountyvotes.gov&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<b>Phone:</b> 415.555.5555</p>
+		</div>	
+	<?php } ?>
 </footer>
 
 	<?php wp_footer(); ?>
