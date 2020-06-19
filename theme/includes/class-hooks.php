3cd786fe0320ff00d0d34946c8152a209ee9da01
legarlin <legarlin@gmail.com>
2020-06-19 16:32:47 -0700

    add tile styling + voting page

--- theme/includes/class-hooks.php
+++ theme/includes/class-hooks.php
@@ -34,7 +34,7 @@ class Hooks {
 
 		wp_enqueue_script( 'main', get_template_directory_uri() . "/assets/js/main.{$type}.js", [ 'jquery', 'underscore', 'wp-util' ], THEME_VERSION, true );
 		wp_enqueue_style( 'main', get_template_directory_uri() . "/assets/css/main.{$type}.css", [], THEME_VERSION );
-		wp_enqueue_style( 'source-sans', 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&display=swap', [], THEME_VERSION );
+		wp_enqueue_style( 'source-sans', 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700&display=swap', [], THEME_VERSION );
 	}
 
 	/**
