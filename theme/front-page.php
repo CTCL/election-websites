58c7b9ad292badbcb5ba55870e7fac3d7ff487c7
legarlin <legarlin@gmail.com>
2020-06-19 16:47:26 -0700

    add tiles to front_page

--- theme/front-page.php
+++ theme/front-page.php
@@ -10,7 +10,18 @@ get_header();
 ?>
 
 <main>
-	Home page
+	<div class="tile-wrapper full-width">
+		<div class="tile">Register to Vote</div>
+		<div class="tile">Vote by Mail</div>
+		<a href="<?php echo esc_url( get_permalink( wpcom_vip_get_page_by_title( 'Results' ) ) ); ?>">
+			<div class="tile">View Election Results</div>
+		</a>
+		<div class="tile">Register to Vote</div>
+		<div class="tile">Register to Vote</div>
+		<div class="tile">Register to Vote</div>
+		<div class="tile">List of Voting Locations</div>
+		<div class="tile">Accessible Voting</div>
+	</div>
 </main>
 
 <?php
