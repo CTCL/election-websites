9808104d3cffe20b979fc1d06d0bdec25584fcd3
legarlin <legarlin@gmail.com>
2020-06-19 15:30:11 -0700

    scaffold registration page with text-block-page template

---
+++ theme/template-text-block-page.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Template Name: Text Block Page
+ *
+ * @package CTCL\ElectionWebsite
+ * @since 1.0.0
+ */
+get_header();
+?>
+
+<main>
+	<h1>Registration</h1>
+	<p>You can check your registration status, update your registration, or register for the first time using the resources below.</p>
+	<section>
+		<h3>Eligibility Criteria</h3>
+		<p>You can register to vote in Nevada County if:</p>
+		<ul>
+			<li>You are a citizen of the United States</li>
+			<li>You reside in Nevada County</li>
+			<li>You will be at least 18 years old at the time of the next election</li>
+			<li>You are not in prison or on parole for the conviction of a felony</li>
+			<li>You have not been found by a court to be mentally incompetent</li>
+		</ul>
+		<h5>Pre-registration</h5>
+		<p>You may pre-Register to vote if you are 16 or 17 years old, and meet all of the other eligibility requirements to vote. You will automatically be registered to vote on your 18th birthday.</p>
+	</section>
+	<section>
+		<h3>Update Your Registration</h3>
+		<p>You need to re-register to vote when:</p>
+		<ul>
+			<li>You move</li>
+			<li>You change your name</li>
+			<li>You change your political party preference</li>
+		</ul>
+		<p>The process to re-register to vote is the same as the process to register to vote.
+			Re-register online or complete a paper voter registration form and mail to our office.
+		</p>
+		<h5>What is the deadline to register or re-register to vote?</h5>
+		<p>In California, the deadline to register to vote for an election is 15 days before
+			each local and statewide election day, so please register early!
+		</p>
+	</section>
+	<section>
+		<h3>Confidential Voter Status</h3>
+		<p>Certain voters facing life-threatening situations may qualify for confidential voter status.
+			For more information, please contact the <a>Safe at Home Program</a> or visit the <a>Secretary of State's website</a>.</p>
+	</section>
+
+</main>
+
+<?php
+get_footer();
