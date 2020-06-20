d7fc70219ff079a1580282ae747a90112a7edefa
legarlin <legarlin@gmail.com>
2020-06-20 11:21:56 -0700

    pull content from page for default template

--- theme/index.php
+++ theme/index.php
@@ -10,7 +10,13 @@ get_header();
 ?>
 
 <main>
-	Default content
+	<h1><?php echo esc_html( get_the_title() ); ?></h1>
+	<?php
+		$page_id   = get_the_ID();
+		$curr_page = get_page( $page_id );
+		$content   = apply_filters( 'the_content', $curr_page->post_content );
+		echo wp_kses_post( $content )
+	?>
 </main>
 
 <?php
