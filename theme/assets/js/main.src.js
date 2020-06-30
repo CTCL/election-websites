1f6183a690fe66fe19f85125a2061513695c5262
Paul Schreiber <paulschreiber@gmail.com>
2020-06-30 15:19:01 -0400

    compile assets

--- theme/assets/js/main.src.js
+++ theme/assets/js/main.src.js
@@ -1,2 +1,6 @@
+function submitContactForm( token ) {
+	document.getElementById( 'recaptcha-token' ).value = token;
+	document.getElementById( 'contact-form' ).submit();
+}
 
 //# sourceMappingURL=main.src.js.map
