fca4982b9d4bfa7b2a610dc7d8e1cd89d419ac64
Paul Schreiber <paulschreiber@gmail.com>
2020-06-30 18:05:38 -0400

    use tables to indent section-block-editor

--- theme/assets/js/src/blocks/numbered-section-block-editor.js
+++ theme/assets/js/src/blocks/numbered-section-block-editor.js
@@ -1,40 +1,40 @@
 var el = wp.element.createElement;
 
 wp.blocks.registerBlockType( 'ctcl-election-website/numbered-section-block', {
-    title: 'Numbered Section',
-    icon: 'editor-ol',
-    category: 'common',
+	title: 'Numbered Section',
+	icon: 'editor-ol',
+	category: 'common',
 
-    edit: function( props ) {
-        return el( 'section',
-            {
-                className: 'numbered-section-block-editor'
-            },
-            el( 'div',
-                {
-                    className: 'numbered-section-block-editor-counter'
-                }
-            ),
-            el( 'div',
-                {
-                    className: 'numbered-section-block-editor-content'
-                },
-                el( wp.blockEditor.InnerBlocks )
-            )
-        );
-    },
+	edit: function( props ) {
+		return el( 'section',
+			{
+				className: 'numbered-section-block-editor'
+			},
+			el( 'div',
+				{
+					className: 'numbered-section-block-editor-counter'
+				}
+			),
+			el( 'div',
+				{
+					className: 'numbered-section-block-editor-content'
+				},
+				el( wp.blockEditor.InnerBlocks )
+			)
+		);
+	},
 
-    save: function( props ) {
-        return el( 'section',
-            {
-                className: 'numbered-section'
-            },
-            el( 'div',
-                {
-                    className: 'numbered-section-content'
-                },
-                el( wp.blockEditor.InnerBlocks.Content )
-            )
-        );
-    }
+	save: function( props ) {
+		return el( 'section',
+			{
+				className: 'numbered-section'
+			},
+			el( 'div',
+				{
+					className: 'numbered-section-content'
+				},
+				el( wp.blockEditor.InnerBlocks.Content )
+			)
+		);
+	}
 } );
