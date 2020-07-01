a0667ad3f4efe13f56b5e0066d3b11caf587130e
Paul Schreiber <paulschreiber@gmail.com>
2020-07-01 14:01:54 -0400

    update contact form to use server-side rendering

--- theme/assets/js/src/blocks/contact-form.js
+++ theme/assets/js/src/blocks/contact-form.js
@@ -1,15 +1,17 @@
 const { registerBlockType } = wp.blocks;
+const { serverSideRender: ServerSideRender } = wp;
 
 registerBlockType( 'ctcl-election-website/contact-form', {
 	title: 'Contact Form',
 	icon: 'email',
 	category: 'election-blocks',
 
-	edit( { className } ) {
-		return <p className={ className }>Hello World, step 2 (from the editor, in green).</p>;
-	},
-
-	save() {
-		return <p>Hello World, step 2 (from the frontend, in red).</p>;
+	edit: function( props ) {
+		return (
+			<ServerSideRender
+				block="ctcl-election-website/contact-form"
+				attributes={ props.attributes }
+			/>
+		);
 	}
 } );
