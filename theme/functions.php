12c017f7033ce9f275218797cb0d6de098c0b4c7
Paul Schreiber <paulschreiber@gmail.com>
2020-07-03 16:07:41 -0400

    clean up functions.php includes

--- theme/functions.php
+++ theme/functions.php
@@ -11,12 +11,18 @@ define( 'THEME_VERSION', '0.5' );
 
 require_once __DIR__ . '/includes/class-helpers.php';
 require_once __DIR__ . '/includes/class-hooks.php';
+
+// blocks
+require_once __DIR__ . '/includes/class-blocks.php';
 require_once __DIR__ . '/includes/class-contact-form.php';
 require_once __DIR__ . '/includes/class-office-info.php';
-require_once __DIR__ . '/includes/class-blocks.php';
-require_once __DIR__ . '/includes/class-settings.php';
-require_once __DIR__ . '/includes/class-google-analytics.php';
-require_once __DIR__ . '/includes/class-google-settings.php';
+
+// settings
 require_once __DIR__ . '/includes/class-banner-settings.php';
+require_once __DIR__ . '/includes/class-google-settings.php';
 require_once __DIR__ . '/includes/class-inquiries-settings.php';
+require_once __DIR__ . '/includes/class-office-details-settings.php';
+
+// google integration
+require_once __DIR__ . '/includes/class-google-analytics.php';
 require_once __DIR__ . '/includes/class-recaptcha.php'; // must be after settings page
