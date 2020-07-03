ed533065bc5135883b57e02a2f63e63000a653ca
Paul Schreiber <paulschreiber@gmail.com>
2020-07-03 19:50:49 -0400

    compile assets

---
+++ theme/assets/js/admin.src.js
@@ -0,0 +1,45 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+	var addButton, removeButtons;
+
+	if ( 'elections_page_topics' !== window.pagenow ) {
+		return;
+	}
+
+	addButton = document.querySelector( '.button.add' );
+	if ( addButton ) {
+		addButton.addEventListener( 'click', addTopic );
+	}
+
+	removeButtons = document.querySelectorAll( '.button.remove' );
+	if ( removeButtons ) {
+		removeButtons.forEach( function( b ) {
+			b.addEventListener( 'click', removeTopic );
+		} );
+	}
+
+	function addTopic( e ) {
+		var lastRow = e.target.parentNode;
+		var lastAddButton = lastRow.querySelector( '.button' );
+		var newRemoveButton = document.createElement( 'input' );
+		var newRow = lastRow.cloneNode( true );
+
+		newRemoveButton.setAttribute( 'type', 'button' );
+		newRemoveButton.setAttribute( 'class', 'button remove' );
+		newRemoveButton.setAttribute( 'value', 'Remove' );
+		newRemoveButton.addEventListener( 'click', removeTopic );
+
+		newRow.querySelector( '.button.add' ).addEventListener( 'click', addTopic );
+		newRow.querySelector( '.multitext' ).value = '';
+		lastRow.appendChild( newRemoveButton );
+		lastAddButton.remove();
+		lastRow.parentNode.appendChild( newRow );
+	}
+
+	function removeTopic( e ) {
+		e.target.parentNode.remove();
+	}
+
+} );
+
+
+//# sourceMappingURL=admin.src.js.map
