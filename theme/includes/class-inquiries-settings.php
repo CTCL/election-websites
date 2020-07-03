05e2d805924ed622670f7eab0f5201f5796c1dbe
Paul Schreiber <paulschreiber@gmail.com>
2020-07-03 19:50:44 -0400

    rename inquiries to settings

--- theme/includes/class-inquiries-settings.php
+++ theme/includes/class-inquiries-settings.php
@@ -1,37 +1,44 @@
 <?php
 namespace CTCL\ElectionWebsite;
 
-class Inquiries_Settings extends Settings {
+class Topics_Settings extends Settings {
 
-	const PAGE_TITLE = 'Inquiries';
-	const FIELD_GROUP = 'inquiries_fields';
+	const PAGE_TITLE = 'Contact Form Topics';
+	const FIELD_GROUP = 'topics_fields';
 
 	public static function register_menu() {
-		add_submenu_page( 'elections', 'Inquiries', 'Inquiries', 'manage_options', 'inquiries', [ get_called_class(), 'page' ] );
+		add_submenu_page( 'elections', 'Topics', 'Topics', 'manage_options', 'topics', [ get_called_class(), 'page' ] );
 	}
 
 	public static function register_settings() {
 		add_settings_section(
-			'inquiries_section',
-			'Inquiries',
+			'topics_section',
+			false,
 			false,
 			static::FIELD_GROUP
 		);
 
 		$fields = [
 			[
-				'uid'         => 'question_list',
-				'label'       => 'Question',
-				'section'     => 'inquiries_section',
-				'type'        => 'text',
+				'uid'         => 'topic_list',
+				'label'       => 'Topics',
+				'section'     => 'topics_section',
+				'type'        => 'multitext',
 				'placeholder' => 'Blah Blah',
-				'label_for'   => 'question_list',
-				'args'        => [ 'sanitize_callback' => 'sanitize_text_field' ],
+				'label_for'   => 'topic_list',
+				'args'        => [ 'sanitize_callback' => [ __CLASS__, 'sanitize_topic_list' ] ],
 			],
 		];
 
 		self::configure_fields( $fields, static::FIELD_GROUP );
 	}
+
+	public static function sanitize_topic_list( $args ) {
+
+		return $args;
+		var_dump($args);
+		exit;
+	}
 }
 
-add_action( 'after_setup_theme', [ '\CTCL\ElectionWebsite\Inquiries_Settings', 'hooks' ] );
+add_action( 'after_setup_theme', [ '\CTCL\ElectionWebsite\Topics_Settings', 'hooks' ] );
