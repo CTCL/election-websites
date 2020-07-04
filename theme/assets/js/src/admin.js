b1b4788fd05bdb0d4669cf08cb72b562043c4507
Paul Schreiber <paulschreiber@gmail.com>
2020-07-03 23:20:16 -0400

    admin js: reactor + add banner image support

--- theme/assets/js/src/admin.js
+++ theme/assets/js/src/admin.js
@@ -1,23 +1,5 @@
-document.addEventListener( 'DOMContentLoaded', function() {
-	var addButton, removeButtons;
-
-	if ( 'elections_page_topics' !== window.pagenow ) {
-		return;
-	}
-
-	addButton = document.querySelector( '.button.add' );
-	if ( addButton ) {
-		addButton.addEventListener( 'click', addTopic );
-	}
-
-	removeButtons = document.querySelectorAll( '.button.remove' );
-	if ( removeButtons ) {
-		removeButtons.forEach( function( b ) {
-			b.addEventListener( 'click', removeTopic );
-		} );
-	}
-
-	function addTopic( e ) {
+window.ctcl = {
+	addTopic: function( e ) {
 		var lastRow = e.target.parentNode;
 		var lastAddButton = lastRow.querySelector( '.button' );
 		var newRemoveButton = document.createElement( 'input' );
@@ -33,11 +15,102 @@ document.addEventListener( 'DOMContentLoaded', function() {
 		lastRow.appendChild( newRemoveButton );
 		lastAddButton.remove();
 		lastRow.parentNode.appendChild( newRow );
-	}
+	},
 
-	function removeTopic( e ) {
+	removeTopic: function( e ) {
 		e.target.parentNode.remove();
+	},
+
+	handleTopics: function() {
+		var addButton, removeButtons;
+
+		addButton = document.querySelector( '.button.add' );
+		if ( addButton ) {
+			addButton.addEventListener( 'click', window.ctcl.addTopic );
+		}
+
+		removeButtons = document.querySelectorAll( '.button.remove' );
+		if ( removeButtons ) {
+			removeButtons.forEach( function( b ) {
+				b.addEventListener( 'click', window.ctcl.removeTopic );
+			} );
+		}
+	},
+
+	handleBanner: function() {
+		var uploadButton = document.getElementById( 'upload_banner_image' );
+		var removeButton = document.querySelector( '.button.remove' );
+
+		if ( uploadButton ) {
+			uploadButton.addEventListener( 'click', window.ctcl.handleBannerUpload );
+		}
+
+		if ( removeButton ) {
+			removeButton.addEventListener( 'click', window.ctcl.removeBannerThumbnail );
+		}
+	},
+
+	removeBannerThumbnail: function( e ) {
+		var imageId = document.getElementById( 'banner_image' );
+		var imageTag = e.target.parentNode.querySelector( 'img' );
+
+		e.target.setAttribute( 'disabled', 'disabled' );
+
+		if ( imageId ) {
+			imageId.value = '';
+		}
+
+		if ( imageTag ) {
+			imageTag.remove();
+		}
+	},
+
+	handleBannerUpload: function( e ) {
+		var mediaUploader;
+		e.preventDefault();
+		if ( mediaUploader ) {
+			mediaUploader.open();
+			return;
+		}
+
+		mediaUploader = wp.media.frames.file_frame = wp.media( {
+			title: 'Choose Image',
+			button: {
+				text: 'Choose Image'
+			}, multiple: false } );
+
+		mediaUploader.on( 'select', function() {
+			var imgTag = e.target.parentNode.querySelector( 'img' );
+			var removeButton = e.target.parentNode.querySelector( '.button.remove' );
+			var newImgTag = document.createElement( 'img' );
+			var attachment = mediaUploader.state().get( 'selection' ).first().toJSON();
+			document.getElementById( 'banner_image' ).value = attachment.id;
+
+			if ( imgTag ) {
+				imgTag.remove();
+			}
+
+			if ( removeButton ) {
+				removeButton.removeAttribute( 'disabled' );
+			}
+
+			newImgTag.setAttribute( 'id', 'banner_image_thumbnail' );
+			newImgTag.setAttribute( 'alt', attachment.title );
+			newImgTag.setAttribute( 'src', attachment.sizes.thumbnail.url );
+			newImgTag.setAttribute( 'width', attachment.sizes.thumbnail.width );
+			newImgTag.setAttribute( 'heigt', attachment.sizes.thumbnail.height );
+
+			e.target.parentNode.appendChild( newImgTag );
+		} );
+
+		mediaUploader.open();
 	}
+};
 
+document.addEventListener( 'DOMContentLoaded', function() {
+	if ( 'elections_page_topics' === window.pagenow ) {
+		window.ctcl.handleTopics();
+	}	else if ( 'elections_page_banner' === window.pagenow ) {
+		window.ctcl.handleBanner();
+	}
 } );
-
