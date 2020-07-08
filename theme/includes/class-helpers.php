43b0ccf77a6586d4b8884a8a7531bb25d2d641db
Paul Schreiber <paulschreiber@gmail.com>
2020-07-08 16:27:35 -0400

    docblocks for helpers

--- theme/includes/class-helpers.php
+++ theme/includes/class-helpers.php
@@ -15,7 +15,11 @@ namespace CTCL\Elections;
  * @since 1.0.0
  */
 class Helpers {
-
+	/**
+	 * List US states and territories.
+	 *
+	 * @return array
+	 */
 	public static function state_list() {
 		return [
 			''   => 'Select a State',
@@ -81,6 +85,11 @@ class Helpers {
 		];
 	}
 
+	/**
+	 * Format US phone numbers.
+	 *
+	 * @return string
+	 */
 	public static function format_phone_number( $p ) {
 		$p = preg_replace( '/[^\d]/', '', $p );
 		if ( ! $p ) {
@@ -90,22 +99,47 @@ class Helpers {
 		return sprintf( '(%s) %s-%s', substr( $p, 0, 3 ), substr( $p, 3, 3 ), substr( $p, 6, 4 ) );
 	}
 
+	/**
+	 * Format 5-digit zip codes.
+	 *
+	 * @return string
+	 */
 	public static function format_zip( $s ) {
 		return substr( preg_replace( '/[^\d]/', '', $s ), 0, 5 );
 	}
 
+	/**
+	 * Format Twitter usernames.
+	 *
+	 * @return string
+	 */
 	public static function format_twitter( $s ) {
 		return substr( preg_replace( '/[^A-Z0-9_]/i', '', $s ), 0, 15 );
 	}
 
+	/**
+	 * Format Facebook usernames.
+	 *
+	 * @return string
+	 */
 	public static function format_facebook( $s ) {
 		return preg_replace( '/[^A-Z0-9\.]/i', '', $s );
 	}
 
+	/**
+	 * Format Instagram usernames.
+	 *
+	 * @return string
+	 */
 	public static function format_instagram( $s ) {
 		return preg_replace( '/[^A-Z0-9_\.]/i', '', $s );
 	}
 
+	/**
+	 * Ensure state is from list of valid state abbreviations.
+	 *
+	 * @return string
+	 */
 	public static function validate_state( $s ) {
 		$state_list = self::state_list();
 
@@ -116,6 +150,11 @@ class Helpers {
 		return '';
 	}
 
+	/**
+	 * Determine if we are in the backend while rendering a block.
+	 *
+	 * @return boolean
+	 */
 	public static function is_block_backend() {
 		return defined( 'REST_REQUEST' ) && true === REST_REQUEST && 'edit' === filter_input( INPUT_GET, 'context', FILTER_SANITIZE_STRING );
 	}
