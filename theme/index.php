91bab79c947c93b63e12d826e167f8d2d310fea4
Paul Schreiber <paulschreiber@gmail.com>
2020-07-22 13:05:33 -0400

    hide title on home page

--- theme/index.php
+++ theme/index.php
@@ -10,7 +10,10 @@ get_header();
 ?>
 
 <main>
+	<?php if ( ! is_front_page() ) : ?>
 	<h1><?php echo esc_html( get_the_title() ); ?></h1>
+	<?php endif; ?>
+
 	<?php
 		$curr_page = get_post();
 	if ( is_object( $curr_page ) ) {
