56d51654f76b1e99a4760885957aeb3a4391e237
legarlin <legarlin@gmail.com>
2020-07-27 22:37:02 -0700

    show tile block preview in editor / use inspector panel for setting values

--- theme/assets/js/src/blocks/tile-nav.js
+++ theme/assets/js/src/blocks/tile-nav.js
@@ -1,8 +1,24 @@
 const { registerBlockType } = wp.blocks;
 const { createElement } = wp.element;
 
+const { InspectorControls, InnerBlocks, RichText, URLInput } = wp.blockEditor;
+const { PanelBody, PanelRow, SelectControl, TextControl } = wp.components;
+
 const PARENT_BLOCK = 'ctcl-election-website/tile-nav-section-block';
 const CHILD_BLOCK = 'ctcl-election-website/tile-nav-block';
+
+const getIconEl = ( attributes ) => {
+	const { icon } = attributes;
+	if ( icon ) {
+		const iconUrl = `${blockEditorVars.baseUrl}/${icon}.svg`;
+		return createElement( 'img', {
+			width: 50,
+			height: 50,
+			src: iconUrl
+		} );
+	}
+	return null;
+};
 
 registerBlockType( PARENT_BLOCK, {
 	title: 'Tile Navigation',
@@ -66,39 +82,79 @@ registerBlockType( CHILD_BLOCK, {
 			props.setAttributes( { icon: value } );
 		}
 
-		return createElement( 'div',
-			{
-				className: 'tile-nav-block-editor'
-			},
-			createElement( wp.blockEditor.URLInput,
-				{
-					onChange: updateLink,
-					value: props.attributes.url,
-					label: 'Link'
-				}
-			),
-			createElement( wp.components.TextControl,
-				{
-					label: 'Label',
-					placeholder: 'Enter label',
-					onChange: updateLabel,
-					value: props.attributes.label
-				}
-			),
-			createElement( wp.components.SelectControl,
-				{
-					onChange: updateIcon,
-					options: [
-						{ value: '', label: 'Select an Icon' },
-						...blockEditorVars.iconOptions.map( option => ( {
-							value: option, label: option
-						} ) )
-					],
-					label: 'Icon',
-					value: props.attributes.icon
-				}
-			)
-		);
+		const { label, icon } = props.attributes;
+		const isEmpty = ! label && ! icon;
+
+		return <div>
+			<InspectorControls>
+				<PanelBody title="Specify tile values" initialOpen={true}>
+					<PanelRow>
+						<TextControl
+							label="Label"
+							placeholder="Enter Label"
+							onChange={updateLabel}
+							value={props.attributes.label} />
+						<URLInput
+							label="Page"
+							value={props.attributes.url}
+							onChange={updateLink} />
+						<SelectControl
+							label="Icon"
+							value={props.attributes.icon}
+							options={[
+								{ value: null, label: 'Select an Icon' },
+								...blockEditorVars.iconOptions.map( option => ( {
+									value: option, label: option
+								} ) )
+							]}
+							onChange={updateIcon}
+						/>
+					</PanelRow>
+				</PanelBody>
+			</InspectorControls>
+			<div className="tile-nav-block-editor">
+				<div className="tile">
+					{isEmpty ? <span className="placeholder">Set tile values in control panel to your right.</span> : [
+						getIconEl( props.attributes ),
+						<span>{label}</span>
+					]}
+				</div>
+			</div>
+		</div>;
+
+		// return createElement( 'div',
+		// 	{
+		// 		className: 'tile-nav-block-editor'
+		// 	},
+		// 	createElement( wp.blockEditor.URLInput,
+		// 		{
+		// 			onChange: updateLink,
+		// 			value: props.attributes.url,
+		// 			label: 'Link'
+		// 		}
+		// 	),
+		// 	createElement( wp.components.TextControl,
+		// 		{
+		// 			label: 'Label',
+		// 			placeholder: 'Enter label',
+		// 			onChange: updateLabel,
+		// 			value: props.attributes.label
+		// 		}
+		// 	),
+		// 	createElement( wp.components.SelectControl,
+		// 		{
+		// 			onChange: updateIcon,
+		// 			options: [
+		// 				{ value: '', label: 'Select an Icon' },
+		// 				...blockEditorVars.iconOptions.map( option => ( {
+		// 					value: option, label: option
+		// 				} ) )
+		// 			],
+		// 			label: 'Icon',
+		// 			value: props.attributes.icon
+		// 		}
+		// 	)
+		// );
 	},
 
 	save: function( props ) {
