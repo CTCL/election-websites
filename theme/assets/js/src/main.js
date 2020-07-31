6085beca29d2f02fd38fd8b530a14c7eb6c5ef00
Paul Schreiber <paulschreiber@gmail.com>
2020-07-31 10:35:16 -0400

    open PDFs in new tabs
    fixes #63

--- theme/assets/js/src/main.js
+++ theme/assets/js/src/main.js
@@ -27,12 +27,23 @@ window.ctcl = {
 document.addEventListener( 'DOMContentLoaded', function() {
 	var mobileMenu = document.querySelector( '.mobile-menu' );
 	var accordionHeaders = document.querySelectorAll( '.accordion-section-header' );
+	var links = document.querySelectorAll( 'a' );
 
+	// Enable the collapsible sections.
 	accordionHeaders.forEach( function( item ) {
 		item.addEventListener( 'click', window.ctcl.handleAccordionClick, { capture: true } );
 	} );
 
+	// Enable the mobile (hamburger) menu.
 	if ( mobileMenu ) {
 		mobileMenu.addEventListener( 'click', window.ctcl.handleMobileMenuClick );
 	}
+
+	// Open PDFs in new tabs.
+	links.forEach( function( link ) {
+		if ( link.href.match( /\.pdf$/ ) ) {
+			link.setAttribute( 'target', '_blank' );
+			link.setAttribute( 'rel', 'noopener noreferrer' );
+		}
+	} );
 } );
