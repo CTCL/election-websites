21b69fe058657009fbbb972a41e8c339959c4a29
Paul Schreiber <paulschreiber@gmail.com>
2020-07-31 12:44:09 -0400

    add mail-error and mail-success templates

---
+++ theme/template-parts/mail-error.php
@@ -0,0 +1,7 @@
+<h1>Uh-oh</h1>
+
+<p>We weren’t able to send your message because of the following error:</p>
+
+<?php if ( $error ) : ?>
+<code><?php echo esc_html( $error ); ?></code>
+<?php endif; ?>
---
+++ theme/template-parts/mail-success.php
@@ -0,0 +1,5 @@
+<h1>Thank you for reaching out!</h1>
+
+<p>We’ve received your message and will respond within X business days. In the meantime, you may find helpful information in our <a href="/faq/">Frequently Asked Questions</a>.</p>
+
+<p><a href="/about-us/">Go back</a></p>
