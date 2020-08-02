1d52d10790a98ee97a303bf68d2f9bd8448fb706
Paul Schreiber <paulschreiber@gmail.com>
2020-08-02 01:49:29 -0400

    add 404 page

---
+++ theme/404.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 404 Template.
+ *
+ * @package CTCL\Elections
+ * @since   1.0.0
+ */
+
+get_header();
+
+$theme = \CTCL\Elections\Elections_Settings::get_theme_slug();
+$file  = get_template_directory() . '/assets/images/icons/' . $theme . '/not-found.svg';
+?>
+
+<main>
+	<?php echo wp_kses_post( \CTCL\Elections\Helpers::inline_svg_tag( $file, 200, 158 ) ); ?>
+
+	<h1>Oops! We can’t find the page you’re looking for.</h1>
+
+	<p>The page has moved or does not exist. Try visiting our <a href="/faq/">FAQs</a> for help or going back to the homepage.</p>
+
+	<p><a class="button" href="/">Back to home</a></p>
+</main>
+
+<?php
+get_footer();
