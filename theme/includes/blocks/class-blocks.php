ebba00f2ed638710231c767ed553bd31259505aa
legarlin <legarlin@gmail.com>
2020-08-03 19:54:16 -0700

    add read more block

--- theme/includes/blocks/class-blocks.php
+++ theme/includes/blocks/class-blocks.php
@@ -122,6 +122,14 @@ class Blocks {
 		);
 
 		register_block_type(
+			'ctcl-election-website/read-more-block',
+			[
+				'editor_script' => 'block-editors',
+				'editor_style'  => 'block-editors',
+			]
+		);
+
+		register_block_type(
 			'ctcl-election-website/contact-form',
 			[
 				'render_callback' => [ '\CTCL\Elections\Contact_Form', 'block_render' ],
