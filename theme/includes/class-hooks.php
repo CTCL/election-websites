169fa8a5632f3b7a57d717ee6622377a96677d0c
Paul Schreiber <paulschreiber@gmail.com>
2020-08-06 18:07:25 -0400

    add_theme_support for automatic-feed-links

--- theme/includes/class-hooks.php
+++ theme/includes/class-hooks.php
@@ -202,6 +202,7 @@ class Hooks {
 		add_theme_support( 'title-tag' );
 		add_theme_support( 'custom-logo' );
 		add_theme_support( 'html5' );
+		add_theme_support( 'automatic-feed-links' );
 	}
 
 	/**
