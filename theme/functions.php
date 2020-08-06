370815995aa5c1d01dd5b9934784d3edc16924ad
Paul Schreiber <paulschreiber@gmail.com>
2020-08-06 16:58:01 -0400

    bump version number

--- theme/functions.php
+++ theme/functions.php
@@ -7,7 +7,7 @@
  */
 
 // Global constants.
-define( 'THEME_VERSION', '0.5' );
+define( 'THEME_VERSION', '0.9' );
 
 $host = filter_input( INPUT_SERVER, 'HTTP_HOST', FILTER_SANITIZE_STRING );
 if ( substr( $host, -strlen( '.test' ) ) === '.test' ) {
