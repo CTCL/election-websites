48b499b52962cc67833193519e26c914572ed13d
Paul Schreiber <paulschreiber@gmail.com>
2020-08-17 21:05:45 -0400

    Update mail-success.php
    fix #119

--- theme/template-parts/mail-success.php
+++ theme/template-parts/mail-success.php
@@ -11,6 +11,6 @@ $file = get_template_directory() . '/assets/images/checkmark.svg';
 <h1><?php echo wp_kses_post( \CTCL\Elections\Helpers::inline_svg_tag( $file, 28, 28 ) ); ?>
 	Thank you for reaching out!</h1>
 
-<p>We’ve received your message and will respond within X business days. In the meantime, you may find helpful information in our <a href="/faq/">Frequently Asked Questions</a>.</p>
+<p>We’ve received your message and will respond as soon as possible. In the meantime, you may find helpful information in our <a href="/faq/">Frequently Asked Questions</a>.</p>
 
 <p><a href="/about-us/">Go back</a></p>
