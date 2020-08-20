27db28ec0162e202b7c9b584b65502feb4dc91ec
Paul Schreiber <paulschreiber@gmail.com>
2020-08-19 23:16:32 -0400

    mark version as 0.8

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
  */
 
 // Global constants.
-define( 'THEME_VERSION', '0.9' );
+define( 'THEME_VERSION', '0.8' );
 
 $host = filter_input( INPUT_SERVER, 'HTTP_HOST', FILTER_SANITIZE_STRING );
 if ( substr( $host, -strlen( '.test' ) ) === '.test' ) {
