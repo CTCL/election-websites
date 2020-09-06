e45f2e6489c9864fa30e205ddc96e082cdbdd228
Paul Schreiber <paulschreiber@gmail.com>
2020-09-06 16:15:43 -0400

    bump version to 0.8.4

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
  */
 
 // Global constants.
-define( 'THEME_VERSION', '0.8.3' );
+define( 'THEME_VERSION', '0.8.4' );
 
 if ( ! defined( 'WP_ENVIRONMENT_TYPE' ) ) {
 	$host = filter_input( INPUT_SERVER, 'HTTP_HOST', FILTER_SANITIZE_STRING );
