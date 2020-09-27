57720cec021e2cce5261fa355803ad151ce7b122
Paul Schreiber <paulschreiber@gmail.com>
2020-09-27 19:22:22 -0400

    change version from 0.9 to 0.9.0 so version comparisons succeed

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
  */
 
 // Global constants.
-define( 'THEME_VERSION', '0.9' );
+define( 'THEME_VERSION', '0.9.0' );
 
 if ( ! defined( 'WP_ENVIRONMENT_TYPE' ) ) {
 	$host = filter_input( INPUT_SERVER, 'HTTP_HOST', FILTER_SANITIZE_STRING );
