a52c314d58da00a0c65e859d36cb67c59eb8e432
Brian Colombini <17318187+brianjcolombini@users.noreply.github.com>
2023-05-03 10:54:22 -0400

    chore: bump version to 0.10.0

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
  */
 
 // Global constants.
-define( 'THEME_VERSION', '0.9.4' );
+define( 'THEME_VERSION', '0.10.0' );
 
 if ( ! defined( 'WP_ENVIRONMENT_TYPE' ) ) {
 	$host = filter_input( INPUT_SERVER, 'HTTP_HOST', FILTER_SANITIZE_STRING );
